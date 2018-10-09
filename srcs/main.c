9d5cf20d63fe13e9fcb41d2ba32804df933964b5
Rupert brax Schuppe <rschuppe@e2r14p6.21-school.ru>
2018-10-09 15:30:11 +0300

    say what?

--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define GET_CELL(field, pos)	((field[pos / 8] >> (8 - pos % 8 - 1)) & 1)
+
 int	main()
 {
+	int pos;
+	int *test;
+
+	test = (int*)malloc(2 * sizeof(int));
+	// [2, 1]
+	pos = 7;
+	test[0] = 0x00;
+	test[1] = 0xFF;
+
+	printf("field[%d][%d] %d", pos / 8, (8 - 1 - pos % 8), GET_CELL(test, pos));
 	return (0);
 }
