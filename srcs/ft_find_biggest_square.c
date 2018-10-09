dd23f58c396794948caa72133a2a8f72a8fe97d6
Rupert brax Schuppe <rschuppe@e2r14p6.21-school.ru>
2018-10-09 17:28:08 +0300

    asd

--- srcs/ft_find_biggest_square.c
+++ srcs/ft_find_biggest_square.c
@@ -10,12 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//  
-
-void    ft_find_biggest_sqare(char *field, int filed_size[2], int start_pos)
+int     is_point_inside_square(int point, int square_lu, int square_rd)
 {
 
+}
 
-    (void)field;
-    (void)filed_size;
+void    ft_find_biggest_square(int *obstacle_map, int field_size[2], int start_pos)
+{
+    int i;
+    int square_size;
+    int diagonal;
+
+    square_size = MAX(field_size[0] - (start_pos % field_size[0]), field_size[1] - (start_pos / field_size[1]));
+
+    i = 0;
+    while (obstacle_map[i] != -1)
+    {
+        diagonal = start_pos + square_size - 1 + (field_size[0] * (square_size - 1));
+        if (is_point_inside_square(obstacle_map[i], start_pos, diagonal))
+        {
+            
+        }
+    }
 }
